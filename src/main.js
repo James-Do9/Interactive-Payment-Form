ae68cab8cc698b30038b84fc0e1d9ce6371eebc6
James-Do9 <dojames3@yahoo.com>
2020-12-08 20:49:01 +0000

    Update

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-/*const errorMsg = str => {
-  var errorMsg = document.querySelector("#errorMsg");
-  errorMsg.classList.contains("d-none") && errorMsg.classList.toggle("d-none");
-  errorMsg.innerHTML += "<li>" + str + "</li>";
-};*/
+const errorMsg = str => {
+  let errorBox = document.querySelector("#errorMsg");
+  errorBox.classList.contains("d-none") && errorBox.classList.toggle("d-none");
+  errorBox.innerHTML += "<li>" + str + "</li>";
+};
 
 window.validation = function validation() {
+  var errorBox = document.querySelector("#errorMsg");
+  errorBox.innerHTML = "";
   var inputCardNumber = document.querySelector("#inputCardNumber");
   var inputCVC = document.querySelector("#inputCVC");
   var inputAmount = document.querySelector("#inputAmount");
@@ -16,37 +18,37 @@ window.validation = function validation() {
   var inputCity = document.querySelector("#inputCity");
   var inputState = document.querySelector("#inputState");
   var inputPostalCode = document.querySelector("#inputPostalCode");
-  var errorMsg = document.querySelector("#errorMsg");
+
   if (inputCardNumber.value < 1000000000000000) {
-    errorMsg.innerHTML = "Please enter the 16 digits of your credit card.";
-    return false;
+    errorMsg("Please enter the 16 digits of your credit card.");
+    inputCardNumber.classList.add("invalid");
   }
   if (inputCVC.value == "") {
-    errorMsg.innerHTML = "Please enter the 3 digits of your CVC.";
-    return false;
+    errorMsg("Please enter the 3 digits of your CVC.");
+    inputCVC.classList.add("invalid");
   }
   if (inputAmount.value == "") {
-    errorMsg.innerHTML = "Please enter the amount.";
-    return false;
+    errorMsg("Please enter the amount.");
+    inputAmount.classList.add("invalid");
   }
-  if (inputFirstName.length < 2) {
-    errorMsg.innerHTML = "Please enter your first name.";
-    return false;
+  if (inputFirstName.value.length < 2) {
+    errorMsg("Please enter your first name.");
+    inputFirstName.classList.add("invalid");
   }
-  if (inputLastName.length < 2) {
-    errorMsg.innerHTML = "Please enter your last name.";
-    return false;
+  if (inputLastName.value.length < 2) {
+    errorMsg("Please enter your last name.");
+    inputLastName.classList.add("invalid");
   }
-  if (inputCity.length < 3) {
-    errorMsg.innerHTML = "Please enter the city you reside in.";
-    return false;
+  if (inputCity.value.length < 3) {
+    errorMsg("Please enter the city you reside in.");
+    inputCity.classList.add("invalid");
   }
   if (inputState.value == "option") {
-    errorMsg.innerHTML = "Please enter the state you reside in.";
-    return false;
+    errorMsg("Please enter the state you reside in.");
+    inputState.classList.add("invalid");
   }
   if (inputPostalCode.value == "") {
-    errorMsg.innerHTML = "Please enter the postal code you reside in.";
-    return false;
+    errorMsg("Please enter the postal code you reside in.");
+    inputPostalCode.classList.add("invalid");
   }
 };
